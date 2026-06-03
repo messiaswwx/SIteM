9558ef38ca852738ee128614c8b0fcdfa2031c6b
Gabriel Messias <Messiaswwx@gmail.com>
2026-06-03 15:14:27 -0300

    Adicionando UseStaticFiles

--- Program.cs
+++ Program.cs
@@ -26,7 +26,10 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
+
+// ADICIONE ESSA LINHA AQUI ABAIXO DO HTTPS REDIRECTION:
 app.UseStaticFiles();
+
 app.UseAntiforgery();
 
 app.MapStaticAssets();
