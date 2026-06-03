6ed9d9d8270b0ac867c29d0e40d89546f4e1c5a8
Gabriel Messias <Messiaswwx@gmail.com>
2026-06-03 14:55:36 -0300

    alteraçao no program

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
